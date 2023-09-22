6aa133ca551a9cdfd65e730f785539973ee065b9
RuthWanzila <wanzilaruth533@gmail.com>
2023-09-21 22:35:25 -0700

    executable shell

--- main.c
+++ main.c
@@ -1,81 +1,50 @@
-#include “main.h”
+#include "main.h"
 /**
- * * main – entry point
- * * @ac: count
- * * @agv: vector
- * * Return: always 0
- * */
-
-/* function declaration */
-int main(int ac, char**argv)
+* main - entry point of program
+* @ac: count
+* @argv: vector
+* Return: always 0
+*/
+int main(int ac, char **argv)
 {
-	/* variable declarations and assignment */
-	char *printout, *LINE_PTR, LINE_PTR1;
-	size_t n;
-	ssize_t ncharacters;
-	const char *delim;
-	int tkn_no.;
-	char tkn;
-	int i;
-	printout = “shell$ “;
-	LINE_PTR = NULL;
-	LINE_PTR1 = NULL;
-	n = 0;
-	delim = “ \n”;
-	tkn_no. = 0;
-	/* void variables */
-	(void)ac;
-	/* loop for shell’s prompt */
-	while (1)
-	{
-		/* print the prompt */
-		printf("%s", printout);
-		/* get output from the user using getline function */
-		ncharacters = getline(&LINE_PTR, &n, stdin);
-		/* if getline fails!!! */
-		if (ncharacters == -1)
-		{
-			/* eline_ptrit shell with CTRL+D */
-			printf("Eline_ptriting shell...\n"); 
-			return (-1);
-		}
-		/* memory allocation to the line pointer */
-		LINE_PTR1 = malloc(sizeof(char) * ncharacters); 
-		/* incase of an error */
-		if (LINE_PTR1 == NULL)
-		{
-			perror("tsh: memory allocation error");
-			return (-1);
-		}
-		/* split the string – line pointer i.e LINE_PTR */
-		tkn = strtok(LINE_PTR, delim);
-
-		while (tkn != NULL)
-		{
-			tkn_no.++;
-			tkn = strtok(NULL, delim);
-		}
-		tkn_no.++;
-		/* memory allocation */
-		argv = malloc(sizeof(char *) * tkn_no.);
-		/* split string */
-		tkn = strtok(LINE_PTR1, delim);
-		/* for loop */
-		for (i = 0; tkn != NULL; i++)
-		{
-			/* memory allocation */
-			argv[i] = malloc(sizeof(char) * strlen(tkn));
-			/* strings copying */
-			strcpy(argv[i], tkn);
-			tkn = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-		/* eline_ptrecute command */
-		eline_ptrecmd(argv);
-	}
-	/* free space */
-	free(LINE_PTR1);
-	free(LINE_PTR);
-	/* return - always 0 */
-	return (0);
+char *printout = "shell$ ", *X = NULL, *X1 = NULL;
+size_t n = 0;
+ssize_t ncharacters;
+const char *Y = " \n";
+int tkn_no = 0, i;
+char *tkn;
+(void)ac;
+while (1)
+{
+/* print the prompt and get output from the user*/
+printf("%s", printout);
+ncharacters = getline(&X, &n, stdin);
+if (ncharacters == -1)
+/* exit shell with CTRL+D */
+printf("Exiting shell...\n");
+return (-1);
+/* memory allocation to the line pointer and incase of an error */
+X1 = malloc(sizeof(char) * ncharacters);
+if (X1 == NULL)
+perror("tsh: memory allocation error");
+return (-1);
+tkn = strtok(X, Y);
+while (tkn != NULL)
+tkn_no++;
+tkn = strtok(NULL, Y);
+tkn_no++;
+/* memory allocation and string splitting*/
+argv = malloc(sizeof(char *) * tkn_no);
+tkn = strtok(X1, Y);
+for (i = 0; tkn != NULL; i++)
+argv[i] = malloc(sizeof(char) * strlen(tkn));
+/* strings copying and execute command */
+strcpy(argv[i], tkn);
+tkn = strtok(NULL, Y);
+argv[i] = NULL;
+execmd(argv);
+}
+free(X1);
+free(X);
+return (0);
 }
--- shell_exec.c
+++ shell_exec.c
@@ -1,30 +1,29 @@
 #include "main.h"
 /**
- * * execmd - execute cmd function
- * *@argv: argument vector
- * *Return: nothing!!
- * */
-
+ * execmd - execute cmd function
+ * @argv: argument vector
+ * Return: nothing!!
+ */
 /* function declaration */
 void execmd(char **argv)
 {
 /* variable declaration and definition/assignment */
-	char *cmd, *init_cmd;
-	cmd = NULL;
-	init_cmd = NULL;
+char *cmd, *init_cmd;
+cmd = NULL;
+init_cmd = NULL;
 /* conditional statement for the argument vector of the function*/
-	if (argv)
-	{
+if (argv)
+{
 /* get the command */
-		cmd = argv[0];
+cmd = argv[0];
 /* get the actual location of the command */
-		init_cmd = get_location(cmd);
+init_cmd = get_location(cmd);
 /* what happens when you use the execute cmd(execve) a*/
-		if (execve(init_cmd, argv, NULL) == -1)
-		{
+if (execve(init_cmd, argv, NULL) == -1)
+{
 /* for an error */
-			perror("Error:");
-		}
-	}
+perror("Error:");
+}
+}
 /* return = void */
 }
